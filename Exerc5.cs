1f1152b90131cb398af47f81245eb5d65a908fa7
Gabriel <127339130+Gtomazini@users.noreply.github.com>
2024-03-23 22:12:57 -0300

    exec5

---
+++ Exerc5.cs
@@ -0,0 +1,19 @@
+﻿
+namespace targetResposta
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //Console.WriteLine("Converse comigo:");
+            string inversora = "teste";
+            int b = inversora.Length;
+            int c = b;
+          
+            for (int i = inversora.Length - 1;i >= 0;  i--)
+            {
+                Console.Write(inversora[i]);
+            }
+        }
+    }
+}
